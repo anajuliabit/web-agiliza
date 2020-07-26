7cbd236db7d31b85307c754182d7f13f3b64fb49
Ana Julia Bittencourt <anajuliabit@gmail.com>
2020-07-26 14:54:15 -0300

    feat: button default

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,69 @@
 import React from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import "./styles.css";
+import logo from "../../assets/logo.svg";
+import { useHistory } from "react-router-dom";
 
 export const Home = () => {
-  return <div>Hello </div>;
+  const navigation = useHistory();
+  function handleLoan() {
+    navigation.push("/simulate");
+  }
+  function handleInvest() {
+    navigation.push("/invest");
+  }
+
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
+  return (
+    <div>
+      <Carousel responsive={responsive}>
+        <div className="item slide1">
+          <img src={logo} className="logo" alt="logo" />
+          <h1>Um novo jeito de realizar seus sonhos.</h1>
+        </div>
+        <div className="item slide2">
+          <img src={logo} className="logo" alt="logo" />
+          <p>Pra quem quer empréstimo:</p>
+          <h1>A gente empresta.</h1>
+        </div>
+        <div className="item slide3">
+          <img src={logo} className="logo" alt="logo" />
+          <p>
+            Pra quem <br></br>quer investir:
+          </p>
+          <h1>A gente também investe.</h1>
+        </div>
+        <div className="item slide4">
+          <img src={logo} className="logo" alt="logo" />
+          <h1>Empréstimo de pessoa para pessoa:</h1>
+          <p>No fim, todos saem ganhando.</p>
+        </div>
+      </Carousel>
+      <div className="content">
+        <button onClick={handleLoan}> Quero pedir um empréstimo</button>
+        <button className="button2" onClick={handleInvest}>
+          Quero investir
+        </button>
+      </div>
+    </div>
+  );
 };
